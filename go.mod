669f3c197040059545d445cd421e56aa13d2d53d
AlexS8332 <alex.etk.dev@gmail.com>
2026-09-13 14:37:51 +0300

    chore: заготовка задания 10-aitrenning-task10

---
+++ go.mod
@@ -0,0 +1,3 @@
+module aitr/10-aitrenning-task10
+
+go 1.27.0
---
+++ main.go
@@ -0,0 +1,8 @@
+// Задание 10 — Управление контекстом: окно, факты и ветки диалога (DeepSeek, Go + браузер)
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("10-aitrenning-task10: заготовка готова")
+}
